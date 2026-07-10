ebf4c56fae0825ca9fc0e4c1947bb4e121b44a74
dv1j <dvijpurmanun1@gmail.com>
2026-07-10 10:44:15 +0400

    removed old path

--- shell.h
+++ shell.h
@@ -5,8 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 extern char **environ;
 
