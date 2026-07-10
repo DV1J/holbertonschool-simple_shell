ae99350994d1979eba3ce32d058b9f7f446a913a
dv1j <dvijpurmanun1@gmail.com>
2026-07-10 20:23:24 +0400

    fixed path by moving stat fall back outside Path check

--- path.c
+++ path.c
@@ -40,12 +40,10 @@ char *get_path(char *command)
 			}
 		}
 		free(path_copy);
-
-		if (stat(command, &buffer) == 0)
-		{
-			return (strdup(command));
-		}
-		return (NULL);
+	}
+	if (stat(command, &buffer) == 0)
+	{
+		return (strdup(command));
 	}
 	return (NULL);
 }
